200bc0f44975774cadb4cec1d35723e0de980761
Jason Lantz <jlantz@innocenceproject.org>
2013-08-26 12:47:22 -0400

    Removed overrides of old views which are now moved to the Donation object

--- ip/theme/fundraising/views.py
+++ ip/theme/fundraising/views.py
@@ -1,8 +1,4 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from collective.salesforce.fundraising.fundraising_campaign import ThankYouView as BaseThankYouView
-from collective.salesforce.fundraising.fundraising_campaign import ThankYouEmail as BaseThankYouEmail
-from collective.salesforce.fundraising.fundraising_campaign import HonoraryEmail as BaseHonoraryEmail
-from collective.salesforce.fundraising.fundraising_campaign import MemorialEmail as BaseMemorialEmail
 from collective.salesforce.fundraising.fundraising_campaign import IFundraisingCampaignPage
 from collective.salesforce.fundraising.donation import ThankYouView as BaseDonationThankYouView
 from collective.salesforce.fundraising.donation import ThankYouEmail as BaseDonationThankYouEmail
@@ -11,34 +7,6 @@ from collective.salesforce.fundraising.donation import MemorialEmail as BaseDona
 from collective.salesforce.fundraising.donation import IDonation
 from ip.theme.fundraising.interfaces import IInnocenceProjectFundraisingTheme
 from five import grok
-
-class ThankYouView(BaseThankYouView, grok.View):
-    grok.context(IFundraisingCampaignPage)
-    grok.require('zope2.View')
-    grok.name('thank-you')
-    grok.template('thank-you')
-    grok.layer(IInnocenceProjectFundraisingTheme)
-
-class ThankYouEmail(BaseThankYouEmail, grok.View):
-    grok.context(IFundraisingCampaignPage)
-    grok.require('zope2.View')
-    grok.name('thank-you-email')
-    grok.template('thank-you-email')
-    grok.layer(IInnocenceProjectFundraisingTheme)
-
-class HonoraryEmail(BaseHonoraryEmail, grok.View):
-    grok.context(IFundraisingCampaignPage)
-    grok.require('zope2.View')
-    grok.name('honorary-email')
-    grok.template('honorary-email')
-    grok.layer(IInnocenceProjectFundraisingTheme)
-
-class MemorialEmail(BaseMemorialEmail, grok.View):
-    grok.context(IFundraisingCampaignPage)
-    grok.require('zope2.View')
-    grok.name('memorial-email')
-    grok.template('memorial-email')
-    grok.layer(IInnocenceProjectFundraisingTheme)
 
 class DonationThankYouView(BaseDonationThankYouView, grok.View):
     grok.context(IDonation)
