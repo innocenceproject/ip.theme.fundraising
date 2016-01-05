286e982d22113e502174d5b98055a3bd705e6928
Chrissy Wainwright <chrissy@sixfeetup.com>
2016-01-05 17:28:00 -0600

    add note to users when they hover over the Submit button that they should not hit the back button refs #1223

--- ip/theme/fundraising/static/js/form_updates.js
+++ ip/theme/fundraising/static/js/form_updates.js
@@ -1,6 +1,11 @@
 (function($) { $(function() {
     
     // Display credit card warning
-    $('.form-buttons.after-amount').append("<p>Your card will be charged</p>");
+    $('.form-buttons.after-amount').append("<p>Your card will be charged<br /><span class='noback'>Please do not hit the back button after clicking submit</span></p>");
+
+    // Add warning about back button
+    $(".form-buttons input[value='Submit']").mouseenter(function() {
+        $(".noback").fadeIn('slow');
+    });
     
 }); })(jQuery);
